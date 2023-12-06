da74557d32a44f9d81022758a6dd2998b5c7293f
Jakob Ecker <jakob.ecker42@gmail.com>
2023-12-06 23:58:59 +0100

    solve day 6 part 2

--- src/main/kotlin/com/github/recke96/aoc/days/Day06.kt
+++ src/main/kotlin/com/github/recke96/aoc/days/Day06.kt
@@ -11,31 +11,31 @@ class Day06 : AoCCommand("day-6") {
         Distance:  9  40  200
     """.trimIndent()
 
-    override val secondDemo: String
-        get() = TODO("Not yet implemented")
+    override val secondDemo = firstDemo
 
     override fun solveFirstPart(input: Sequence<String>): String {
         val races = parseRaces(input)
 
         return races.map { it.findWinningAccelerationTimes() }
             .map { it.last - it.first + 1 }
-            .reduce(Int::times)
+            .reduce(Long::times)
             .toString()
     }
 
     override fun solveSecondPart(input: Sequence<String>): String {
-        TODO("Not yet implemented")
+        val race = parseRace(input)
+
+        val winningAccelerations = race.findWinningAccelerationTimes()
+        val numberOfWinningAccelerations = winningAccelerations.last - winningAccelerations.first + 1
+
+        return numberOfWinningAccelerations.toString()
     }
-
-
 }
 
-
-data class DistanceRace(val time: Int, val record: Int)
-
+data class DistanceRace(val time: Long, val record: Long)
 
 // Find both accelerations that allow us to reach the record distance
-fun DistanceRace.findWinningAccelerationTimes(): IntRange {
+fun DistanceRace.findWinningAccelerationTimes(): LongRange {
     val time = time.toDouble()
     // fudge a little since we need to beat the record, not equal it
     val record = record.toDouble() + 1e-6
@@ -43,8 +43,8 @@ fun DistanceRace.findWinningAccelerationTimes(): IntRange {
     val common = sqrt((time * time) - (4.0 * record))
 
     // Winning accelerations
-    val lower = ceil((time - common) / 2.0).toInt().let { if (it == this.record) it + 1 else it }
-    val upper = floor((time + common) / 2.0).toInt().let { if (it == this.record) it - 1 else it }
+    val lower = ceil((time - common) / 2.0).toLong().let { if (it == this.record) it + 1 else it }
+    val upper = floor((time + common) / 2.0).toLong().let { if (it == this.record) it - 1 else it }
 
     return lower..upper
 }
@@ -53,8 +53,25 @@ fun parseRaces(input: Sequence<String>): List<DistanceRace> {
     val numberRegex = Regex("""\d+""")
     val (timesInput, distancesInput) = input.toList();
 
-    val times = numberRegex.findAll(timesInput).map { it.value.toInt() }
-    val distances = numberRegex.findAll(distancesInput).map { it.value.toInt() }
+    val times = numberRegex.findAll(timesInput).map { it.value.toLong() }
+    val distances = numberRegex.findAll(distancesInput).map { it.value.toLong() }
 
     return times.zip(distances).map { DistanceRace(it.first, it.second) }.toList()
+}
+
+fun parseRace(input: Sequence<String>): DistanceRace {
+    val numberRegex = Regex("""\d+""")
+    val (timeInput, distanceInput) = input.toList();
+
+    val time = numberRegex.findAll(timeInput)
+        .map { it.value }
+        .joinToString(separator = "")
+        .toLong()
+
+    val distance = numberRegex.findAll(distanceInput)
+        .map { it.value }
+        .joinToString(separator = "")
+        .toLong()
+
+    return DistanceRace(time, distance)
 }
